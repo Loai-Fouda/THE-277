7c2ee38b41d5784fb30937a4595934ecae2c45f3
Loai-Fouda <loaibelal2000@gamil.com>
2026-05-03 01:14:04 +0300

    add the bank

---
+++ game.py
@@ -0,0 +1,35 @@
+
+import random
+
+WORD_BANK = [
+    # Programming
+    ("PYTHON",      "Programming Language"),
+    ("VARIABLE",    "Programming Concept"),
+    ("FUNCTION",    "Programming Concept"),
+    ("ALGORITHM",   "Computer Science"),
+    ("DATABASE",    "Computer Science"),
+    ("COMPILER",    "Computer Science"),
+    ("RECURSION",   "Programming Concept"),
+    ("ITERATOR",    "Programming Concept"),
+    ("EXCEPTION",   "Programming Concept"),
+    ("BOOLEAN",     "Data Type"),
+    # Engineering
+    ("VOLTAGE",     "Electronics"),
+    ("CIRCUIT",     "Electronics"),
+    ("RESISTOR",    "Electronics"),
+    ("TRANSISTOR",  "Electronics"),
+    ("FREQUENCY",   "Physics"),
+    ("AMPLITUDE",   "Physics"),
+    # General
+    ("KEYBOARD",    "Computer Hardware"),
+    ("MONITOR",     "Computer Hardware"),
+    ("NETWORK",     "Computer Science"),
+    ("SOFTWARE",    "Computer Science"),
+    ("HARDWARE",    "Computer Science"),
+    ("INTERNET",    "Technology"),
+    ("BROWSER",     "Technology"),
+    ("FRAMEWORK",   "Programming"),
+    ("LIBRARY",     "Programming"),
+]
+
+MAX_WRONG_ATTEMPTS = 6    
