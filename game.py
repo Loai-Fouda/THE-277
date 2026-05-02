f2c735ab51ae63a36150b95b3a15ffbb8368d2ee
Kareem <oelgaml353@gmail.com>
2026-05-03 02:14:57 +0300

    start new game

--- game.py
+++ game.py
@@ -33,3 +33,33 @@ WORD_BANK = [
 ]
 
 MAX_WRONG_ATTEMPTS = 6    
+class HangmanGame:
+
+    def start_new_game(self) -> dict:
+        """Pick a random word and return a fresh game state."""
+        word, category = random.choice(WORD_BANK)
+        return self._build_initial_state(word, category)
+
+    def process_guess(self, state: dict, letter: str) -> dict:
+
+        state = dict(state)   
+
+        if state["game_over"] or letter in state["guessed_letters"]:
+            return state
+
+        state["guessed_letters"].append(letter)
+
+        if letter in state["word"]:
+            state["correct_letters"].append(letter)
+            state["last_guess_correct"] = True
+        else:
+            state["wrong_letters"].append(letter)
+            state["wrong_count"] += 1
+            state["last_guess_correct"] = False
+
+        state["display_word"] = self._build_display_word(
+            state["word"], state["correct_letters"]
+        )
+
+        state = self._check_game_over(state)
+        return state
